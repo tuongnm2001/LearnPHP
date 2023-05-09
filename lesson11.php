c0ce8a1c8d2f1a7a7073f1baef321ee9051533a6
Tuong Nguyen <tuongnm2001@gmail.com>
2023-05-09 11:33:18 +0700

    lesson 11

---
+++ lesson11.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson 11</title>
+</head>
+<body>
+    <h3>Lesson 11</h3>
+    <?php 
+       $a = 8 ;
+       $b = 4 ;
+
+       if($a > $b){
+        echo "a is larger than b";
+       }else if($a < $b){
+        echo "a is smaller than b ";
+       }else{
+        echo "a is equal b ";
+       }
+    ?>
+
+</body>
+</html> 
