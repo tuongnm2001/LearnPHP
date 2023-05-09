f79c42728d668984791a325b4572f50bf85ca68a
Tuong Nguyen <tuongnm2001@gmail.com>
2023-05-09 11:10:12 +0700

    lesson9

---
+++ lesson9.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson 8</title>
+</head>
+<body>
+    <h3>Lesson 9</h3>
+    <?php 
+        // $max_width = 980 ;
+
+        define('MAX_WIDTH' , 980);
+        echo MAX_WIDTH
+    ?>
+    <br/>
+
+    <?php 
+        define('MAX_WIDTH', 981);
+        echo MAX_WIDTH
+    ?>
+    
+    
+
+</body>
+</html> 
